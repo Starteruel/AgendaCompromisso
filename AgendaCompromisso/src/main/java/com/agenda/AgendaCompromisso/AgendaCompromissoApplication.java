7d24dd88caa0e790632932c21381997580c752c0
starteruel <starteruel@gmail.com>
2024-12-07 20:52:28 -0300

    Commit 1

---
+++ AgendaCompromisso/src/main/java/com/agenda/AgendaCompromisso/AgendaCompromissoApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.AgendaCompromisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCompromissoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCompromissoApplication.class, args);
+	}
+
+}
